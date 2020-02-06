6ca3dbe0189c68296d0dc9f2e6f0da8deb66ffcc
deepika1133 <khamithkardeepika1133@gmail.com>
2020-02-06 23:42:54 +0530

    first

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+  void method1()
+{
+  System.out.println("welcome");
+}
+  public static void main(String args[])
+{
+Hello h=new Hello();
+System.out.println("hi");
+h.method1();
+}
+}
